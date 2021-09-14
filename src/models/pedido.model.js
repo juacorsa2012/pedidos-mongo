40a2d672cae63bea965e5a059b043cf1cc0ac98c
juacorsa2012 <juacorsa2012@gmail.com>
2021-09-14 18:34:21 +0200

    Se añade estado enum en el modelo pedido

--- src/models/pedido.model.js
+++ src/models/pedido.model.js
@@ -8,48 +8,41 @@ const pedidoSchema = new mongoose.Schema(
     required: [true, Mensaje.PRODUCTO_REQUERIDO],    
     trim: true            
   },
-
   unidades: {
     type: Number,
     required: [true, Mensaje.UNIDADES_REQUERIDO]
   },
-
   referencia: {
     type: String,    
     trim: true        
   },
-
   oferta: {
     type: String,    
     trim: true        
   },
-
   observaciones: {
     type: String,    
     trim: true        
   },
-
   parte: {
     type: String,    
     trim: true        
   },
-
   estado: {
-    type: Number    
+    type: String,
+    enum : ['pedido','preparado','entregado','facturado','devuelto'],
+    default: 'pedido' 
   },
-
   created_at: {
     type: Date,
     default: Date.now(),
     select: false
   },
-
   cliente: {
     type: mongoose.Schema.ObjectId,
     ref: 'Cliente',
     required: [true, Mensaje.CLIENTE_REQUERIDO]
   },
-
   proveedor: {
     type: mongoose.Schema.ObjectId,
     ref: 'Proveedor',
