de6080986c459d6f94448805071f11438213f51a
juacorsa2012 <juacorsa2012@gmail.com>
2021-09-17 11:20:25 +0200

    Se redefinen las constantes y los mensajes

--- src/config/constantes.js
+++ src/config/constantes.js
@@ -1,2 +1,7 @@
-export const ROL_ADMIN = 'admin'
-export const ROL_USER  = 'usuario'
+const ROL_ADMIN = 'admin'
+const ROL_USER  = 'usuario'
+
+export {
+  ROL_ADMIN,
+  ROL_USER
+}
--- src/config/mensajes.js
+++ src/config/mensajes.js
@@ -1,8 +1,17 @@
-export const SUCCESS = 'success'
-export const ERROR   = 'error'
-export const SERVIDOR_CORRIENDO  = 'Servidor corriendo en el puerto'
-export const NOMBRE_REQUERIDO    = 'El nombre es un campo requerido'
-export const PRODUCTO_REQUERIDO  = 'El producto es un campo requerido'
-export const UNIDADES_REQUERIDO  = 'Las unidades es un campo requerido'
-export const CLIENTE_REQUERIDO   = 'El cliente es un campo requerido'
-export const PROVEEDOR_REQUERIDO = 'El proveedor es un campo requerido'
+const SUCCESS = 'success'
+const ERROR   = 'error'
+const NOMBRE_REQUERIDO    = 'El nombre es un campo requerido'
+const PRODUCTO_REQUERIDO  = 'El producto es un campo requerido'
+const UNIDADES_REQUERIDO  = 'Las unidades es un campo requerido'
+const CLIENTE_REQUERIDO   = 'El cliente es un campo requerido'
+const PROVEEDOR_REQUERIDO = 'El proveedor es un campo requerido'
+
+export {
+  SUCCESS,
+  ERROR,
+  NOMBRE_REQUERIDO,
+  PRODUCTO_REQUERIDO,
+  UNIDADES_REQUERIDO,
+  CLIENTE_REQUERIDO,
+  PROVEEDOR_REQUERIDO  
+}
