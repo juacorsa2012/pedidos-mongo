458ee7256b0ac25da852c646075ccd967e9304e7
juacorsa2012 <juacorsa2012@gmail.com>
2021-10-01 08:59:24 +0200

    Se añade proveedor.controller.test.js

--- src/tests/integration/cliente.controller.test.js
+++ src/tests/integration/cliente.controller.test.js
@@ -7,12 +7,15 @@ import { CLIENTE_ACTUALIZADO, CLIENTE_REGISTRADO, NOMBRE_REQUERIDO, SUCCESS } fr
 
 const url = '/api/v1/clientes/'
 
-describe('/api/v1/clientes', () => {
-  describe('GET /', () => {
-    beforeAll(async () => {          
+describe('${url}', () => {
+  let cliente1
+  let cliente2
+
+  beforeEach(async () => {          
       await Cliente.deleteMany({})
-      await Cliente.create({ nombre: 'Cliente 1'})  
-    })
+      cliente1 = await Cliente.create({ nombre: 'Cliente 1'})  
+      cliente2 = await Cliente.create({ nombre: 'Cliente 2'})  
+  })
 
     it("debe devolver todos los clientes", async () => {        
       const res = await request(app).get(url)
@@ -20,82 +23,58 @@ describe('/api/v1/clientes', () => {
       expect(res.statusCode).toBe(StatusCode.OK)
       expect(res.body.status).toBe(SUCCESS)
       expect(res.body.results).toBeDefined()            
-      expect(res.body.results).toBe(1)          
-      expect(res.body.data.clientes[0]['nombre']).toBe('Cliente 1')          
+      expect(res.body.results).toBe(2)                        
     })  
-  })
-   
-  describe('GET /:id', () => {
-      let cliente
 
-      beforeAll(async () => {
-        await Cliente.deleteMany({})
-        cliente = await Cliente.create({ nombre: 'Cliente 1'})  
-      })
+    it("debe devolver un cliente", async() => {
+      const res = await request(app).get(url + cliente1._id)
 
-      it("debe devolver un cliente", async() => {
-        const res = await request(app).get(url + cliente._id)
+      expect(res.statusCode).toBe(StatusCode.OK)
+      expect(res.body.status).toBe(SUCCESS)           
+      expect(res.body.data.cliente.nombre).toBe(cliente1.nombre)        
+      expect(res.body.data.cliente._id).toBeDefined()         
+    })
 
-        expect(res.statusCode).toBe(StatusCode.OK)
-        expect(res.body.status).toBe(SUCCESS)           
-        expect(res.body.data.cliente.nombre).toBe(cliente.nombre)        
-        expect(res.body.data.cliente._id).toBeDefined()         
-      })
+    it("debe devolver un error 404 si el id está mal formado", async() => {
+      const id = mongoose.Types.ObjectId()
+      const res = await request(app).get(url + id)
 
-      it("debe devolver un error 404 si el id está mal formado", async() => {
-        const id = mongoose.Types.ObjectId()
-        const res = await request(app).get(url + id)
+      expect(res.statusCode).toBe(StatusCode.NOT_FOUND)
+    })
 
-        expect(res.statusCode).toBe(StatusCode.NOT_FOUND)
-      })
+    it("debe devolver un error 400 si no existe el cliente", async() => {
+      const res = await request(app).get(url + '1')
 
-      it("debe devolver un error 400 si no existe el cliente", async() => {
-        const res = await request(app).get(url + '1')
+      expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)    
+    })  
 
-        expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)    
-      })  
-  })   
-  
-  describe('GET /:id', () => {
-      let cliente = { nombre: "Cliente 1" }
+    it("debe devolver un cliente", async () => {
+      const cliente = {nombre: 'Cliente 3'}
+      const res = await request(app).post(url).send(cliente)
 
-      beforeAll(async () => {
-        await Cliente.deleteMany({})
-      })
+      expect(res.statusCode).toBe(StatusCode.CREATED)
+      expect(res.body.status).toBe(SUCCESS)
+      expect(res.body.message).toBe(CLIENTE_REGISTRADO)
+      expect(res.body.data.cliente.nombre).toBe(cliente.nombre)   
+      expect(res.body.data.cliente._id).toBeDefined()        
+    })
 
-      it("debe devolver un cliente", async () => {
-        const res = await request(app).post(url).send(cliente)
+    it("debe devolver un error 400 cuando no se facilita el nombre del cliente", async () => {
+      const cliente = {nombre: ''}
+      const res = await request(app).post(url).send(cliente)
 
-        expect(res.statusCode).toBe(StatusCode.CREATED)
-        expect(res.body.status).toBe(SUCCESS)
-        expect(res.body.message).toBe(CLIENTE_REGISTRADO)
-        expect(res.body.data.cliente.nombre).toBe(cliente.nombre)   
-        expect(res.body.data.cliente._id).toBeDefined()        
-      })
+      expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
+      expect(res.body.success).toBeFalsy()
+      expect(res.body.error).toBe(NOMBRE_REQUERIDO)
+    })
 
-      it("debe devolver un error 400 cuando no se facilita el nombre del cliente", async () => {
-        cliente.nombre = ""
-        const res = await request(app).post(url).send(cliente)
+    it("debe devolver un error 400 si el cliente ya existe", async () => {        
+      const cliente = {nombre: 'Cliente 1'}
+      await request(app).post(url).send(cliente)
+      const res = await request(app).post(url).send(cliente)
 
-        expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
-        expect(res.body.success).toBeFalsy()
-        expect(res.body.error).toBe(NOMBRE_REQUERIDO)
-      })
-
-      it("debe devolver un error 400 si el cliente ya existe", async () => {        
-        await request(app).post(url).send(cliente)
-        const res = await request(app).post(url).send(cliente)
-
-        expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
-        expect(res.body.success).toBeFalsy()
-      })
-  })
-
-  describe('GET /count', () => {
-    beforeAll(async () => {
-      await Cliente.deleteMany({})
-      await Cliente.create({ nombre: 'Cliente 1' })  
-      await Cliente.create({ nombre: 'Cliente 2' })  
+      expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
+      expect(res.body.success).toBeFalsy()
     })
 
     it("debe contar los clientes registrados en la base de datos", async() => {
@@ -104,24 +83,10 @@ describe('/api/v1/clientes', () => {
       expect(res.statusCode).toBe(StatusCode.OK)
       expect(res.body.data.count).toBe(2)
     })
-  })
   
-  describe('PUT /:id', () => {
-    let idCliente1
-    let idCliente2
-    let cliente
-
-    beforeAll(async () => {
-      await Cliente.deleteMany({})
-      const cliente1 = await Cliente.create({ nombre: 'Cliente 1' })  
-      const cliente2 = await Cliente.create({ nombre: 'Cliente 2' })  
-      idCliente1 = cliente1._id
-      idCliente2 = cliente2._id
-    })
-
     it("debe actualizar un cliente con éxito", async() => {
-      cliente = { nombre: "Cliente 1" }
-      const res = await request(app).put(url + idCliente1).send(cliente)
+      const cliente = { nombre: "Cliente 999" }
+      const res = await request(app).put(url + cliente1._id).send(cliente)
 
       expect(res.statusCode).toBe(StatusCode.OK)
       expect(res.body.status).toBe(SUCCESS)   
@@ -131,13 +96,15 @@ describe('/api/v1/clientes', () => {
     })  
 
     it("debe dar un error 400 si actualizamos un cliente sin nombre", async() => {
-      cliente = { nombre: "" }
-      const res = await request(app).put(url + idCliente1).send(cliente)
+      const cliente = { nombre: "" }
+      const res = await request(app).put(url + cliente1._id).send(cliente)
 
       expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)
       expect(res.body.success).toBeFalsy()    
       expect(res.body.error).toBe(NOMBRE_REQUERIDO)        
     })  
 
-  }) 
-})
+  
+  
+  
+  })
---
+++ src/tests/integration/proveedor.controller.test.js
@@ -0,0 +1,115 @@
+import request from 'supertest'
+import mongoose from 'mongoose'
+import StatusCode from 'http-status-codes'
+import Proveedor from '../../models/proveedor.model.js'
+import app from '../../app.js'
+import { CLIENTE_ACTUALIZADO, CLIENTE_REGISTRADO, ERROR, NOMBRE_REQUERIDO, PROVEEDOR_ACTUALIZADO, PROVEEDOR_REGISTRADO, SUCCESS } from '../../config/mensajes.js'
+
+const url = '/api/v1/proveedores/'
+
+describe('${url}', () => {
+  let proveedor1 
+  let proveedor2
+
+  beforeEach(async () => {          
+    await Proveedor.deleteMany({})
+    proveedor1 = await Proveedor.create({ nombre: 'Proveedor 1'})  
+    proveedor2 = await Proveedor.create({ nombre: 'Proveedor 2'})  
+  })
+
+  it("debe devolver todos los proveedores", async () => {        
+    const res = await request(app).get(url)
+      
+    expect(res.statusCode).toBe(StatusCode.OK)
+    expect(res.body.status).toBe(SUCCESS)
+    expect(res.body.results).toBeDefined()            
+    expect(res.body.results).toBe(2)          
+    expect(res.body.data.proveedores[0]['nombre']).toBe('Proveedor 1')          
+  })
+  
+  it("debe devolver un proveedor", async() => {
+    const res = await request(app).get(url + proveedor1._id)
+
+    expect(res.statusCode).toBe(StatusCode.OK)
+    expect(res.body.status).toBe(SUCCESS)           
+    expect(res.body.data.proveedor.nombre).toBe(proveedor1.nombre)        
+    expect(res.body.data.proveedor._id).toBeDefined()         
+  })
+
+  it("debe devolver un error 404 si el id está mal formado", async() => {
+    const id = mongoose.Types.ObjectId()
+    const res = await request(app).get(url + id)
+
+    expect(res.statusCode).toBe(StatusCode.NOT_FOUND)
+  })
+
+  it("debe devolver un error 400 si no existe el proveedor", async() => {
+    const res = await request(app).get(url + '1')
+
+    expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)    
+  })  
+
+  it("debe devolver un proveedor", async () => {
+    const proveedor = { nombre: "Proveedor" }
+    const res = await request(app).post(url).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.CREATED)
+    expect(res.body.status).toBe(SUCCESS)
+    expect(res.body.message).toBe(PROVEEDOR_REGISTRADO)
+    expect(res.body.data.proveedor.nombre).toBe(proveedor.nombre)   
+    expect(res.body.data.proveedor._id).toBeDefined()        
+  })
+
+  it("debe devolver un error 404 si duplicamos el nombre de un proveedor", async () => {
+    const proveedor = { nombre: "Proveedor 1" }
+    const res = await request(app).post(url).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)
+    expect(res.body.error).toBeDefined()
+    expect(res.body.success).toBeFalsy()        
+  })
+
+  it("debe devolver un error 400 cuando no se facilita el nombre del proveedor", async () => {
+    const proveedor = { nombre: "" }
+    const res = await request(app).post(url).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
+    expect(res.body.success).toBeFalsy()
+    expect(res.body.error).toBe(NOMBRE_REQUERIDO)
+  })
+
+  it("debe devolver un error 400 al registrar un proveedor existente", async () => {        
+    const proveedor = { nombre: "Proveedor 1" }
+    const res = await request(app).post(url).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)      
+    expect(res.body.success).toBeFalsy()
+  })
+
+  it("debe contar los proveedores registrados en la base de datos", async() => {
+    const res = await request(app).get(url + '/count')
+
+    expect(res.statusCode).toBe(StatusCode.OK)
+    expect(res.body.data.count).toBe(2)
+  })
+
+  it("debe actualizar un proveedor con éxito", async() => {
+    const proveedor = { nombre: "Proveedor 999" }
+    const res = await request(app).put(url + proveedor2._id).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.OK)
+    expect(res.body.status).toBe(SUCCESS)   
+    expect(res.body.message).toBe(PROVEEDOR_ACTUALIZADO)
+    expect(res.body.data.proveedor._id).toBeDefined()
+    expect(res.body.data.proveedor.nombre).toBe(proveedor.nombre)
+  })  
+
+  it("debe dar un error 400 si actualizamos un proveedor sin nombre", async() => {
+    const proveedor = { nombre: "" }
+    const res = await request(app).put(url + proveedor1._id).send(proveedor)
+
+    expect(res.statusCode).toBe(StatusCode.BAD_REQUEST)
+    expect(res.body.success).toBeFalsy()    
+    expect(res.body.error).toBe(NOMBRE_REQUERIDO)        
+  })  
+})
